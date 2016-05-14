d5f40ff150210c838d99d069cee41c675874d63d
谢仁洪 <xierenyuan@qq.com>
2016-05-14 23:49:58 +0800

    ..

--- app/_home/home.module.js
+++ app/_home/home.module.js
@@ -5,11 +5,9 @@
  * @date 16/5/14
  */
 import HomeConfig from './config/home.config';
-console.log('11呵呵哒');
 let HomeModule = angular.module('materialApp.Home', []);
 
 HomeModule.config(HomeConfig);
 
-console.log('呵呵哒');
 
 export default HomeModule = HomeModule.name;
