ead95a7d292467645275e22eff99f616f301e0e4
谢仁洪 <xierenyuan@qq.com>
2016-06-01 16:58:48 +0800

    ..

--- app/_home/config/home.config.js
+++ app/_home/config/home.config.js
@@ -20,29 +20,6 @@ class HomeConfig {
         //默认首页
         $urlRouterProvider.otherwise("/home");
     }
-
-    constructor($stateProvider, $urlRouterProvider) {
-       // 'ngInject';
-        this.$inject = ['$stateProvider', '$urlRouterProvider'];
-        this.$stateProvider = $stateProvider;
-        this.$urlRouterProvider = $urlRouterProvider;
-        this.routerInit();
-    }
-
-    /**
-     * 初始化 路由
-     */
-    routerInit() {
-        console.log('router init');
-        this.$stateProvider.state('home', {
-            url: '/home',
-            templateUrl: homeTmp,
-           // controller: "HomeController as home"
-        });
-
-        //默认首页
-        this.$urlRouterProvider.otherwise("/home");
-    }
 }
 
 export default HomeConfig.routerInit;
